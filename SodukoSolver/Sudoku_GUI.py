c2005c3c451a9b4f0ca1cf2faa20f85e6ade5629
masonthegit <masonpbecker@gmail.com>
2021-09-07 15:05:11 -0700

    Add files via upload

--- SodukoSolver/Sudoku_GUI.py
+++ SodukoSolver/Sudoku_GUI.py
@@ -21,15 +21,15 @@ pygame.font.init()
 class Grid:
 # The First Sudoku Board
     board1 = [
-        [7,8,0,4,0,0,1,2,0],
-        [6,0,0,0,7,5,0,0,9],
-        [0,0,0,6,0,1,0,7,8],
-        [0,0,7,0,4,0,2,6,0],
-        [0,0,1,0,5,0,9,3,0],
-        [9,0,4,0,6,0,0,0,5],
-        [0,7,0,3,0,0,0,1,2],
-        [1,2,0,0,0,7,4,0,0],
-        [0,4,9,2,0,6,0,0,7]
+        [2,0,0,0,0,6,5,7,0],
+        [4,9,0,0,2,7,0,0,1],
+        [0,0,0,0,0,0,2,0,3],
+        [5,4,0,0,7,3,0,9,0],
+        [7,0,6,0,0,0,3,0,4],
+        [0,8,0,2,6,0,0,1,5],
+        [6,0,9,0,0,0,0,0,0],
+        [8,0,0,9,4,0,0,3,6],
+        [0,1,4,6,0,0,0,0,7]
     ]
 
 # The Second Sudoku Board
@@ -62,7 +62,7 @@ class Grid:
     board_list = [board1, board2, board3]
     board = random.choice(board_list)
 
-# The Basic Values
+# The Basic Values For Board Class
     def __init__(self, rows, cols, width, height):
         self.rows = rows
         self.cols = cols
@@ -72,9 +72,11 @@ class Grid:
         self.model = None
         self.selected = None
 
+# Updates The Board
     def update_model(self):
         self.model = [[self.cubes[i][j].value for j in range(self.cols)] for i in range(self.rows)]
 
+# Makes The Temporary Value A Permanent Value If Valid
     def place(self, val):
         row, col = self.selected
         if self.cubes[row][col].value == 0:
@@ -89,12 +91,13 @@ class Grid:
                 self.update_model()
                 return False
 
+# Adds The Temporary Number Value You Input
     def sketch(self, val):
         row, col = self.selected
         self.cubes[row][col].set_temp(val)
 
-    def draw(self, win):
 # Draw Grid Lines
+    def draw(self, win):
         gap = self.width / 9
         for i in range(self.rows+1):
             if i % 3 == 0 and i != 0:
@@ -109,8 +112,8 @@ class Grid:
             for j in range(self.cols):
                 self.cubes[i][j].draw(win)
 
+# Selects The Square You Clicked On
     def select(self, row, col):
- # Reset all other
         for i in range(self.rows):
             for j in range(self.cols):
                 self.cubes[i][j].selected = False
@@ -118,16 +121,14 @@ class Grid:
         self.cubes[row][col].selected = True
         self.selected = (row, col)
 
+# Clears Temporary Values
     def clear(self):
         row, col = self.selected
         if self.cubes[row][col].value == 0:
             self.cubes[row][col].set_temp(0)
 
+# Returns The Position Of Where You Clicked On The GUI
     def click(self, pos):
-        """
-        :param: pos
-        :return: (row, col)
-        """
         if pos[0] < self.width and pos[1] < self.height:
             gap = self.width / 9
             x = pos[0] // gap
@@ -136,6 +137,7 @@ class Grid:
         else:
             return None
 
+# Checks If The Board Is Filled
     def is_finished(self):
         for i in range(self.rows):
             for j in range(self.cols):
@@ -143,11 +145,12 @@ class Grid:
                     return False
         return True
 
-
+# Class That Creates The Cubes
 class Cube:
     rows = 9
     cols = 9
 
+# Basic Values For Cube Class
     def __init__(self, value, row, col, width ,height):
         self.value = value
         self.temp = 0
@@ -157,26 +160,32 @@ class Cube:
         self.height = height
         self.selected = False
 
+# Draws The GUI
     def draw(self, win):
         fnt = pygame.font.SysFont("comicsans", 40)
 
+# Basic Values For Draw
         gap = self.width / 9
         x = self.col * gap
         y = self.row * gap
 
+# Color Of Temporary Number
         if self.temp != 0 and self.value == 0:
             text = fnt.render(str(self.temp), 1, (0, 0, 0))
             win.blit(text, (x+5, y+5))
+
+# Color Of Permanent Value
         elif not(self.value == 0):
             text = fnt.render(str(self.value), 1, (0, 0, 0))
             win.blit(text, (x + (gap/2 - text.get_width()/2), y + (gap/2 - text.get_height()/2)))
 
+# Color Of Selected Square
         if self.selected:
             pygame.draw.rect(win, (150, 255, 0), (x,y, gap ,gap), 3)
 
+# Defines Values For Temporary And Permanent
     def set(self, val):
         self.value = val
-
     def set_temp(self, val):
         self.temp = val
 
@@ -196,16 +205,17 @@ def redraw_window(win, board, time, strikes):
 # Draw Grid and Board
     board.draw(win)
 
-# Time
+# Sets The Values For Time
 def format_time(secs):
     sec = secs % 60
     minute = secs // 60 
     hour = minute // 60
 
+# Formats The Time
     mat = "{:01d}:{:02d}:{:02d}".format(hour, minute, sec)
     return mat
 
-
+# Main Function For The Program
 def main():
     win = pygame.display.set_mode((540,600))
     pygame.display.set_caption("Sudoku")
@@ -215,12 +225,14 @@ def main():
     start = time.time()
     strikes = 0
     while run:
-
         play_time = round(time.time() - start)
 
+# Quits The Game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+# Keys For Number Input
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_1:
                     key = 1
@@ -240,9 +252,13 @@ def main():
                     key = 8
                 if event.key == pygame.K_9:
                     key = 9
+
+# Deletes Temporary Number Value
                 if event.key == pygame.K_DELETE:
                     board.clear()
                     key = None
+
+# Enters Number Value As Permanent Value If Valid
                 if event.key == pygame.K_RETURN:
                     i, j = board.selected
                     if board.cubes[i][j].temp != 0:
@@ -253,6 +269,7 @@ def main():
                             strikes += 1
                         key = None
 
+# Messages For Solving The Board Based On Strike Count
                         if board.is_finished():
                             print("You Win")
                             if strikes == 0:
@@ -265,6 +282,7 @@ def main():
                                 print("Can You Do Better?")
                             run = False
 
+# Gets The Position On The GUI You Clicked On
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 clicked = board.click(pos)
@@ -275,9 +293,10 @@ def main():
         if board.selected and key != None:
             board.sketch(key)
 
+# Redraws The GUI Based On Any Changes On Values
         redraw_window(win, board, play_time, strikes)
         pygame.display.update()
 
-
+# Runs The Program
 main()
 pygame.quit()
